1a2cff952fc810bd076c94287aebeb8aee1608ed
Austin Burdette <austinburdette@MC-FX29JQW031.local>
2024-07-17 08:15:25 -0400

    removed project urls

--- setup.py
+++ setup.py
@@ -50,9 +50,5 @@ setup(
     long_description=long_description,
     zip_safe=True,
     python_requires=">=3.7",
-    project_urls={
-        "Bug Reports": "https://github.com/pytube/pytube/issues",
-        "Read the Docs": "https://pytube.io",
-    },
     keywords=["youtube", "download", "video", "stream",],
 )
